faa77433cfc87ba3375e20b05cbf1b30a9a592ce
pksrach <pksrach@gmail.com>
2024-04-24 02:00:24 +0700

    Update dashboard view

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,7 @@
+@include('backend.layouts.header')
+
+<main id="main" class="main">
+    @yield('content')
+</main>
+
+@include('backend.layouts.footer')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Manage Backend Routes
+// Need to use middleware auth to protect the routes
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('backend.dashboard');
+
 });
