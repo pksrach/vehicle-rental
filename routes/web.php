cdd875479cc7f87fcd4dfac3499d95123f05f3cd
pksrach <pksrach@gmail.com>
2024-05-12 13:21:10 +0700

    Remove command code not use

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ use App\Http\Controllers\frontend\ContactController;
 use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\frontend\PricingController;
 use App\Http\Controllers\frontend\ServiceController;
-use App\Http\Controllers\frontend\auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 // Route Auth for Admin
@@ -33,12 +32,6 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 // Index route need to check in not login state to redirect to login page else redirect to dashboard
-//Route::get('/admin', function () {
-//    if (auth()->check()) {
-//        return redirect()->route('backend.dashboard');
-//    }
-//    return redirect()->route('backend.login');
-//});
 Route::group(['prefix' => 'admin', 'middleware' => 'guest'], function () {
     Route::get('/login', [AuthController::class, 'backendLogin'])->name('backend.login');
     Route::post('/login', [AuthController::class, 'backendDoLogin'])->name('backend.login.post');
@@ -135,7 +128,7 @@ Route::middleware([AuthenticateBackendController::class, 'auth'])->group(functio
 Route::group(['prefix' => ''], function () {
     //Auth
     Route::get('/login', [LoginController::class, 'login'])->name('front.login');
-   
+
     // Home
     Route::get('/', [HomeController::class, 'index'])->name('frontend.home');
     // About
